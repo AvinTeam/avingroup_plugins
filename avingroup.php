fa845568a18d7e685eac92f391643445f476dc5a
Mohammadreza <rashidpour1995@gmail.com>
2025-08-31 10:32:47 +0330

    add primary file

---
+++ avingroup.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Avin Group
+ *
+ * Plugin Name: آوین گروپ
+ * Plugin URI:  https://updater.mrrashidpour.com/
+ * Description: افزونه برای سایت آورن گروپ
+ * Version:     1.0.0
+ * Author:      Mohammadreza Rashidpour Aghamahali
+ * Author URI:  https://mrrashidpour.com/
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Requires at least: 6.5
+ * Requires PHP: 7.4
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation. You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+defined('ABSPATH') || exit;
+date_default_timezone_set('Asia/Tehran');
+
+preg_match('/Version:\s*(.+)/i', file_get_contents(__FILE__), $versionMatches);
+
+$version = $versionMatches[ 1 ] ?? "1.0.0";
+
+define('AG_VERSION', $version);
+
+define('AG_FILE', __FILE__);
+define('AG_PATH', plugin_dir_path(__FILE__));
+define('AG_INCLUDES', AG_PATH . 'includes/');
+define('AG_CONFIG', AG_PATH . 'config/');
+define('AG_VIEWS', AG_PATH . 'views/');
